e020cb510132e95dd48dc5c2bf22aa4bd38bc554
Luis Navarro <pozhiin@hotmail.com>
2024-07-07 23:10:06 -0400

    included dotenv package

--- client/client.js
+++ client/client.js
@@ -1,6 +1,7 @@
 import React from 'react'; 
 import App from './App'
 import { createRoot } from 'react-dom/client';
+require('dotenv').config();
 
 const root = createRoot(document.getElementById('root'));
 root.render (
